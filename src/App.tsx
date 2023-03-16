338cae4b5bdec0a413480a1a4e0c326111f76e8e
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-16 09:17:24 +0700

    Advice component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
+import axios, { AxiosError } from 'axios'
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import divider from './assets/images/pattern-divider-desktop.svg'
+import dividerMobile from './assets/images/pattern-divider-mobile.svg'
+import dice from './assets/images/icon-dice.svg';
 
+function App() {
+  const [advice, setAdvice] = useState('');
+  const [adviceId, setAdviceId] = useState('');
+
+  const generateAdvice = async() => {
+    const data = axios.get("https://api.adviceslip.com/advice");
+    data.then(newAdvice => {
+      setAdvice(newAdvice.data.slip.advice);
+      setAdviceId(newAdvice.data.slip.id);
+    }).catch((error: AxiosError) => {
+      if (error.response) {
+        alert("Status: " + error.response.status);
+      } else if (error.request) {
+        alert(error.request);
+      } else {
+        alert('Error: '+ error.message);
+      }
+    })
+  }
+
+  useEffect(() => {
+    generateAdvice();
+  }, [])
 
   return (
-    <div></div>
+    <section>
+      <label htmlFor="advice">Advice #{ adviceId }</label>
+      <h1 id="advice">"{ advice }"</h1>
+      <img id='dividerDesktop' src={ divider } alt="Divider Desktop"></img>
+      <img id='dividerMobile' src={ dividerMobile } alt="Divider Mobile"></img>
+      <button type="button" onClick={ generateAdvice }>
+        <img src={ dice } alt="Dice"></img>
+      </button>
+    </section>
   )
 }
 
